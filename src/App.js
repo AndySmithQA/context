a37ec717f85581e645e1f904d87c9e65fcf81bef
AndySmithQA <andy.smith@qa.com>
2023-03-06 09:22:28 +0000

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CartList from './components/CartList'
+import ItemsList from './components/ItemsList'
+import { CartProvider } from './context/cart-context'
+import './style.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>React Fruit Market</h1>
+      <CartProvider>
+        <CartList />
+        <ItemsList />
+      </CartProvider>
+    </main>
+  )
 }
-
-export default App;
